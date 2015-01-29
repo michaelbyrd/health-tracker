7b999d1bc4cef4597a6278ca94ae96569f9dd75d
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-29 14:39:56 -0500

    meals model and controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,18 @@
 class MealsController < ApplicationController
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private def meal_params
+    params.require(:meal).permit(:amount, :description, :type)
+  end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,5 @@
 class Meal < ActiveRecord::Base
+  validates :amount, presence: true
+  validates :description, presence: true
+  validates :type, presence:true
 end
