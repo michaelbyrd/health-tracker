acf6a1e56c455cc1a225b9c0410339165d5122fd
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-29 14:35:24 -0500

    weight model and controller

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,18 @@
 class WeightsController < ApplicationController
+  def new
+    @weight = Weight.new
+  end
+
+  def create
+    @weight = Weight.new(weight_params)
+    if @weight.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private def weight_params
+    params.require(:weight).permit(:amount, :unit)
+  end
 end
--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,2 +1,4 @@
 class Weight < ActiveRecord::Base
+  validates :amount, presence:true
+  validates :unit, presence:true
 end
