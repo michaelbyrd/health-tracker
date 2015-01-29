11b1c0602db335445e50feb60b731e3c3dee13c5
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-29 17:23:11 -0500

    add all forms to the welcome/index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @weights = Weight.all
     @steps = Step.all
+    @meals = Meal.all
+    @exercises = Exercise.all
+    @meal = Meal.new
+    @step = Step.new
+    @weight = Weight.new
+    @exercise = Exercise.new
 
   end
 end
